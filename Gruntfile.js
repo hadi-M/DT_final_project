e53559d2de89d950ddc1560b93a4422314c068d0
hadi-M <modareshadi@gmail.com>
2016-01-06 23:22:48 +0330

    Done with the initial loading element

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
 	
 	grunt.initConfig({
-		sass: {
+		/*sass: {
 			dist: {
 				options: {
 					style: 'expanded'
@@ -10,17 +10,37 @@ module.exports = function(grunt) {
 					'Home/style.css': 'Home/sass/style.scss',
 				}
 			}
+		}, */
+		compass: {
+			home: {
+				options: {
+					sassDir: 'Home/sass',
+					cssDir: 'Home',
+					noLineComments: true
+					//environment: 'production'
+				}
+			},
+			other: {
+				options: {
+					sassDir: 'sass',
+					cssDir: 'css'
+				}
+			}
 		},
 		watch: {
-			scripts: {
-				files: ['**/*.scss'],
-				tasks: ['sass'],
+			home: {
+				files: ['Home/sass/style.scss'],
+				tasks: ['compass:home'],
 			},
 		},
+		ali: {
+		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks("grunt-jsbeautifier");
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.registerTask('default', ['watch'])
+	//grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('folan', ['ali']);
 };
--- Home/script.js
+++ Home/script.js
@@ -1,232 +1,100 @@
-$(document).ready(function (){
-	var holder = Array(3000);
-	var signal = Array(1000);
-	var shift_amount;
-	var step_counter=0;
-	var correlation=Array(2000);
-	function start() {
-		setTimeout( function(){
-			$("main h1").addClass("animated");
-		}, 1000 );
-		function load_graph() {
-			$('#step-3 .content-hldr #graph-hldr-small').highcharts({
-				chart: {
-					type: 'spline'
-				},
-				title: {
-					text: 'The correlation graph'
-				},
-				xAxis: {
-					type: 'Shift amount (z)',
-					labels: {
-						overflow: 'justify'
-					}
-				},
-				yAxis: {
-					title: {
-						text: 'S(t)*S(t-z)'
-					},
-					minorGridLineWidth: 0,
-					gridLineWidth: 0,
-					alternateGridColor: null,
-				},
-				plotOptions: {
-					spline: {
-						lineWidth: 4,
-						states: {
-							hover: {
-								lineWidth: 5
-							}
-						},
-						marker: {
-							enabled: false
-						},
-					}
-				},
-				series: [ {
-					name: 'Correlation',
-					data: correlation
-				}],
-				navigation: {
-					menuItemStyle: {
-						fontSize: '10px'
-					}
-				}
-			});
-		};
+var s = Snap("#anim");
+var fade;
 
-		function array_sum(array) {
-			var sum=0;
-			for(i=0; i<array.length; i++) {
-				sum+=array[i]
-			}
-			return sum;
+function loader(dist, radius, speed, delay, mode) {
+	var dist = dist;
+	var radius = radius;
+	var speed = speed;
+	var delay = delay;
+	var mode = mode;
+
+	$("#anim").css("width", dist);
+	$("#anim").css("height", dist);
+
+	circles = [];
+	circles[0] = s.circle(radius, radius, radius);
+	circles[1] = s.circle(dist-radius, radius, radius);
+	circles[2] = s.circle(dist-radius, dist-radius, radius);
+	circles[3] = s.circle(radius, dist-radius, radius);
+
+	circles[0].attr({
+		fill: "#EC1C24"
+	});
+	circles[1].attr({
+		fill: "#545457"
+	});
+	circles[2].attr({
+		fill: "#919496"
+	});
+	circles[3].attr({
+		fill: "#F2664D"
+	})
+
+	var position = new Array(2);
+	position[0] = new Array(4);
+	position[1] = new Array(4);
+
+	for (var i=0; i< circles.length; i++) {
+		position[0][i] = circles[i].attr("cx");
+		position[1][i] = circles[i].attr("cy");
+	}
+
+	var turn=mode;
+
+	Loop = function() {
+
+		for (var i=0; i< circles.length; i++) {
+			circles[i].animate({
+				cx: position[0][(i+turn)%4],
+				cy: position[1][(i+turn)%4]
+			}, speed);
 		}
+		turn=(turn+mode+4)%4;
 
-		function load_zoomed_graph() {
-			$('#container').highcharts({
-				chart: {
-					type: 'spline'
-				},
-				title: {
-					text: 'The correlation graph'
-				},
-				xAxis: {
-					type: 'Shift amount (z)',
-					labels: {
-						overflow: 'justify'
-					}
-				},
-				yAxis: {
-					title: {
-						text: 'S(t)*S(t-z)'
-					},
-					minorGridLineWidth: 0,
-					gridLineWidth: 0,
-					alternateGridColor: null,
-				},
-				plotOptions: {
-					spline: {
-						lineWidth: 4,
-						states: {
-							hover: {
-								lineWidth: 5
-							}
-						},
-						marker: {
-							enabled: false
-						},
-					}
-				},
-				series: [ {
-					name: 'Correlation',
-					data: correlation
-				}],
-				navigation: {
-					menuItemStyle: {
-						fontSize: '10px'
-					}
-				}
-			});
-		};
-
-		function show_modal() {
-			$("#black-bg").fadeIn(200, function(){
-				$("#modal").fadeIn(200);
-				$("#container").fadeIn(200);
-			});
-		}
-
-		function hide_modal() {
-			$("#modal").fadeOut(200, function(){
-				$("#container").fadeOut(200);
-				$("#black-bg").fadeOut(200);
-			});
-			// $(".close-icon").unbind("click");
-		}
-
-		function reset_holder() {
-			for (i=0; i<3000; i++) {
-				holder[i]=0;
+		setInterval(function(){		
+			for (var i=0; i< circles.length; i++) {
+				circles[i].animate({
+					cx: position[0][(i+turn)%4],
+					cy: position[1][(i+turn)%4]
+				}, speed);
 			}
-		}
+			turn=(turn+mode+4)%4;
+		}, delay+speed);	
+	}
+	Loop();
 
-		function generate_signal() {
-			for(i=0; i<1000; i++) {
-				signal[i] = (Math.floor(Math.random()*1000))%2;
+	end = function(speed, delay, mode){
+		$.each(circles, function(i, e){
+			setTimeout(function() {
+				circles[i].animate({
+					opacity: 0
+				}, speed)
+			}, delay*(i+1));
+			if (mode == 1) {
+				setTimeout(function() {
+					circles[3-i].animate({
+						opacity: 0
+					}, speed)
+				}, delay*(i+1));
 			}
-		};
-
-		function generate_shift_amount() {
-			shift_amount = (Math.floor(Math.random()*10000))%2000;
-		};
-		function do_shifting() {
-			for(i=0; i<1000; i++) {
-				holder[i+shift_amount]=signal[i];
-			}
-		}
-
-		function calculate() {
-			var temp=Array(1000)
-			for(var shft=0; shft<2000; shft++) {
-				for(i=0; i<1000; i++) {
-					temp[i]=holder[i+shft]*signal[i];
-					if(shft==23 && i==980) {
-						debugger;
-					}
-				}
-				correlation[shft]=array_sum(temp);
-			}
-		}
-
-		function disable_btn(btn) { // 1: play, 2: repeat
-			if (btn == 1) {
-				$(".fa-play")
-				.removeClass("active")
-				.addClass("disable")
-				.unbind("click");
-			}
-			if (btn == 2) {
-				$(".fa-repeat")
-				.removeClass("active")
-				.addClass("disable")
-				.unbind("click");
-			}
-		};
-
-		function enable_btn(btn) {
-			if (btn==1) {
-				$(".fa-play")
-				.addClass("active")
-				.removeClass("disable")
-				.click(function(){
-					if (step_counter==0) {
-						$("#step-1 .step-number").addClass("selected");
-						$("#step-1").addClass("selected");
-						generate_signal();
-						$("#step-1 .tick").addClass("done");
-						step_counter+=1;
-					}
-					else if(step_counter==1) {
-						step_counter+=1;
-						$("#step-1").removeClass("selected");
-						$("#step-2 .step-number").addClass("selected");
-						$("#step-2").addClass("selected");
-						generate_shift_amount();
-						$("#step-2 span").append(shift_amount);
-						reset_holder();
-						do_shifting();
-						$("#step-2 .tick").addClass("done");
-					}
-					else if(step_counter==2) {
-						load_graph();
-						$("#step-2").removeClass("selected");
-						$("#step-3 .step-number").addClass("selected");
-						$("#step-3").addClass("selected");
-						$("#step-3 .tick").addClass("done");
-						$(".zoom").addClass("selected");
-						calculate();
-						$(".zoom.selected").click(function(){
-							show_modal();
-							load_zoomed_graph()
-						});
-						$(".close-icon").click(function(){
-							hide_modal();
-						});
-					}
-				});
+		});
+	}
 }
-if (btn == 2) {
-	$(".fa-repeat")
-	.addClass("active")
-	.removeClass("disable")
-	.click();
 
-}
-};
+loader(dist=80, radius=15, speed=500, delay=0, mode=3);
+// setTimeout(function(){end(200, 500, 3);}, 3000);
 
-enable_btn(1);
-};
-start();
 
+$(window).on("load", function() {
+	end(200, 500, 3);
+	setTimeout(function() {
+		$(".loading").fadeOut();
+	}, 2500 );
+	var Initial_annimation  = function(){
+		$(".title h1").addClass("animated");
+	};
+	setTimeout(function() {
+		Initial_annimation();
+	}, 3500 );
+	
 });
